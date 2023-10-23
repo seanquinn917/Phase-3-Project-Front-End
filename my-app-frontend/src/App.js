882d289461d365b4b89c03c690fad522a9f7c1c9
SeanQuinn <Seanquinn917@gmail.com>
2023-10-23 11:05:15 -0400

    update file name App

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -20,18 +20,6 @@ const displayHotelList = hotels.map((hotel)=>{
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         {displayHotelList}
       </header>
       
